8f54e25e74cee45bca71e4150dcf31c355626c29
xietao91 <xietao98@gmail.com>
2023-02-23 23:04:30 +0800

    新增struct

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -8,9 +8,25 @@ contract SimpleStorage {
     // string str = 'str';
     // address myAdd = 0xE4a32511E65e5d3D5FaBf55F0ae9371FA4c58f5f;
     uint256 public favoriteNumber; // this gets initialized to zero!
+    People public person = People({
+        favoriteNumber: 20,
+        name:'zhangsan'
+    });
+
+    uint256[] favoriteNumberList;
+    People[] public personList;
 
     function store(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
+    }
+
+    function retrive() public view returns(uint256) {
+        return favoriteNumber;
+    }
+
+    struct People {
+        uint256 favoriteNumber;
+        string name;
     }
 
 
